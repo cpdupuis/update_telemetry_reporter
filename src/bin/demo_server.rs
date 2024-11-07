1d62d8e3ffd1ed81d3d844ca1e3e7dddc2f9db06
Chris DuPuis <cdupuis@mozilla.com>
2024-11-07 09:02:35 -0800

    Server seems to accept data

--- src/bin/demo_server.rs
+++ src/bin/demo_server.rs
@@ -1,5 +1,38 @@
-fn main() {
-    println!("This is going to be a demo server eventually");
+use warp::Filter;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Sample {
+    name: Option<String>,
+    quest: Option<String>,
+    captial_of_assyria: Option<String>,
+    favorite_color: Option<String>,
+    airspeed_velocity_of_an_unladen_swallow: Option<u32>
+}
+
+async fn respond(
+    sample:Sample
+    ) -> Result<impl warp::Reply, warp::Rejection> {
+    // Some JSON input data as a &str. Maybe this comes from the user.
+
+    Ok(warp::reply::json(&sample))
+}
+
+#[tokio::main]
+async fn main() {
+
+
+
+    // POST /sample
+    let promote = warp::post()
+        .and(warp::path("sample"))
+        .and(warp::path::end())
+        // Only accept bodies smaller than 16kb...
+        .and(warp::body::content_length_limit(1024 * 16))
+        .and(warp::body::json())
+        .and_then(respond);
+
+    warp::serve(promote).run(([127, 0, 0, 1], 3030)).await
 }
 
 fn hello_there() -> &'static str {
