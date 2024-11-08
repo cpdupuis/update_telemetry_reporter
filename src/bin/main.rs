75b993ffc8bc7a76ff147cbbb6ee0b47fa68a06f
Chris DuPuis <cdupuis@mozilla.com>
2024-11-08 07:24:44 -0800

    Try to configure glean

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,9 +1,7 @@
 use update_telemetry_reporter;
 
 fn main() {
-    println!("Hello, world!");
-    let packet = update_telemetry_reporter::collect_data();
-    println!("version to install is {}", packet.update.version_to_install);
+    update_telemetry_reporter::configure_glean();
     let num = update_telemetry_reporter::report_state();
     let mut answer = String::new();
     match num {
--- src/lib.rs
+++ src/lib.rs
@@ -1,82 +1,18 @@
+use std::path::PathBuf;
+use tempfile::Builder;
+use serde::{Deserialize, Serialize};
+use flate2::read::GzDecoder;
+use glean::{net, ClientInfoMetrics, ConfigurationBuilder, ErrorType};
+use crate::metrics::updater::{self, CompletionCheckObject,CompletionCheckObjectItemUpdateObject,CompletionCheckObjectItemInstallationObject};
+
+
 mod metrics {
     include!(concat!(env!("OUT_DIR"), "/glean_metrics.rs"));
 }
 
-pub enum BasicStatus {
-    Success,
-    Error,
-}
-
-pub enum UpdateType {
-    Patch,
-    Full,
-    Recovery
-}
-
-pub struct ProfileSample {
-    pub last_version: Option<String>
-}
-
-pub struct UpdateSample {
-    pub install_status: BasicStatus,
-    pub rollback_status: Option<BasicStatus>,
-    pub previous_installation_version: Option<String>,
-    pub version_to_install: String,
-    pub update_type: UpdateType,
-    pub seconds_since_update: Option<u32>,
-    pub using_maintenance_service: bool,
-    pub maintenance_service_version: Option<String>
-}
-
-struct InstallationSample {
-    pub is_shared: bool,
-    pub launch_succeeded_since_last_update: bool,
-    pub launch_failed_since_last_update: bool
-}
-
-pub struct TelemetryPacket {
-    pub profile: ProfileSample,
-    pub update: UpdateSample,
-    pub installation: InstallationSample
-}
-
-fn collect_profile_sample() -> ProfileSample {
-    ProfileSample {
-        last_version: Some(String::from("1.2.3.4"))
-    }
-}
-
-fn collect_update_sample() -> UpdateSample {
-    UpdateSample {
-        install_status: BasicStatus::Success,
-        rollback_status: None,
-        previous_installation_version: Some(String::from("0.0.0.1")),
-        version_to_install: String::from("2.0.0.0"),
-        update_type: UpdateType::Patch,
-        seconds_since_update: Some(1234u32),
-        using_maintenance_service: true,
-        maintenance_service_version: Some(String::from("1.1.1.1"))
-    }
-}
-
-fn collect_installation_sample() -> InstallationSample {
-    InstallationSample {
-        is_shared: true,
-        launch_failed_since_last_update: false,
-        launch_succeeded_since_last_update: true
-    }
-}
-
-pub fn collect_data() -> TelemetryPacket {
-    TelemetryPacket {
-        profile: collect_profile_sample(),
-        update: collect_update_sample(),
-        installation: collect_installation_sample()
-    }
-}
-
 pub fn report_state() -> Option<i32> {
-    use crate::metrics::updater::{CompletionCheckObject,CompletionCheckObjectItemUpdateObject,CompletionCheckObjectItemInstallationObject};
+
+
     let report = CompletionCheckObject {
         profile_last_version: Some(String::from("hello there")),
         update: Some(CompletionCheckObjectItemUpdateObject {
@@ -99,13 +35,22 @@ pub fn report_state() -> Option<i32> {
     Some(42)
 }
 
+pub fn configure_glean() {
+    let root = Builder::new().prefix("simple-db").tempdir().unwrap();
+    let data_path: PathBuf = root.path().to_path_buf();
+    let cfg = ConfigurationBuilder::new(true, data_path, "org.mozilla.glean_core.example")
+        .with_server_endpoint("localhost:3030/glean")
+        .with_use_core_mps(false)
+        .build();
+
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_collect_data() {
-        let telemetry_packet = collect_data();
-        assert_eq!(true, telemetry_packet.update.using_maintenance_service);
+    fn test_tautology() {
+        assert_eq!(true, true);
     }
 }
