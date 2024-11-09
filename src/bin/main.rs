ac2ce1ba5167837c6a89a38f13dc225c76a5b77a
Chris DuPuis <cdupuis@mozilla.com>
2024-11-08 16:14:43 -0800

    Through a solid day of thrashing, discovered that the default uploader was a no-op

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,7 @@
 use update_telemetry_reporter;
 
 fn main() {
+    env_logger::init();
     update_telemetry_reporter::configure_glean();
     let num = update_telemetry_reporter::report_state();
     let mut answer = String::new();
@@ -9,4 +10,5 @@ fn main() {
         Some(val) => answer.push_str(&val.to_string  ())
     }
     println!("The number is {}", &answer);
+    update_telemetry_reporter::send_ping();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,41 @@
-use std::path::PathBuf;
-use tempfile::Builder;
-use serde::{Deserialize, Serialize};
-use flate2::read::GzDecoder;
+use crate::metrics::updater::{
+    CompletionCheckObject, CompletionCheckObjectItemInstallationObject,
+    CompletionCheckObjectItemUpdateObject,
+};
+use crate::metrics::updater_check;
 use glean::{net, ClientInfoMetrics, ConfigurationBuilder, ErrorType};
-use crate::metrics::updater::{self, CompletionCheckObject,CompletionCheckObjectItemUpdateObject,CompletionCheckObjectItemInstallationObject};
-
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
+use std::thread;
+use std::time::Duration;
+use tempfile::Builder;
 
 mod metrics {
     include!(concat!(env!("OUT_DIR"), "/glean_metrics.rs"));
 }
 
+
+/// A simple mechanism to upload pings over HTTPS.
+#[derive(Debug)]
+pub struct MyHttpUploader {
+    hello_there: String
+}
+
+impl net::PingUploader for MyHttpUploader {
+    /// Uploads a ping to a server.
+    ///
+    /// # Arguments
+    ///
+    /// * `upload_request` - the requested upload.
+    fn upload(&self, upload_request: net::PingUploadRequest) -> net::UploadResult {
+        // haha. You thought this was an uploader
+        println!("hello_there: {}", self.hello_there);
+        net::UploadResult::http_status(200)
+    }
+}
+
+
 pub fn report_state() -> Option<i32> {
-
-
     let report = CompletionCheckObject {
         profile_last_version: Some(String::from("hello there")),
         update: Some(CompletionCheckObjectItemUpdateObject {
@@ -23,25 +46,67 @@ pub fn report_state() -> Option<i32> {
             is_patch: Some(true),
             time_since_update: Some(65536),
             mms_version: Some(String::from("11")),
-            used_mms: Some(true)
+            used_mms: Some(true),
         }),
         installation: Some(CompletionCheckObjectItemInstallationObject {
             is_shared: Some(true),
             launch_failed: Some(false),
-            launch_succeeded: Some(false)
+            launch_succeeded: Some(false),
         }),
     };
+
     metrics::updater::completion_check.set(report);
     Some(42)
 }
 
+pub fn send_ping() {
+
+    /*updater_check.test_before_next_submit(move |reason| {
+        println!("yippee");
+        match reason {
+            None => assert!(false),
+            Some(val) => println!("reason is {}", val)
+        }
+    });
+    let metric_value = metrics::updater::completion_check.test_get_value("updater-check");
+    match metric_value {
+        None => assert!(false),
+        Some(str) => println!("Metric: {}", str)
+    }
+*/
+    updater_check.submit(Some("because"));
+    thread::sleep(Duration::from_millis(1000));
+    glean::shutdown();
+}
+
 pub fn configure_glean() {
     let root = Builder::new().prefix("simple-db").tempdir().unwrap();
+    match root.path().to_str() {
+        Some(str) => println!("This is root dir: {}", str),
+        None => println!("No root"),
+    }
+    let uploader = MyHttpUploader {
+        hello_there: String::from("General Kenobi")
+    };
     let data_path: PathBuf = root.path().to_path_buf();
-    let cfg = ConfigurationBuilder::new(true, data_path, "org.mozilla.glean_core.example")
-        .with_server_endpoint("localhost:3030/glean")
-        .with_use_core_mps(false)
+    let cfg = ConfigurationBuilder::new(true, data_path, "org.mozilla.updater_report_sample")
+        .with_server_endpoint("https://incoming.telemetry.mozilla.org")
+        .with_use_core_mps(true)
+        .with_uploader(uploader)
         .build();
+
+    let client_info = ClientInfoMetrics {
+        app_build: env!("CARGO_PKG_VERSION").to_string(),
+        app_display_version: env!("CARGO_PKG_VERSION").to_string(),
+        channel: Some(String::from("pre-alpha")),
+        locale: Some(String::from("en-us")),
+    };
+
+    glean::set_log_pings(true);
+    assert!(glean::set_debug_view_tag("cdupuis-updater"));
+    glean::initialize(cfg, client_info);
+
+
 
 }
 
